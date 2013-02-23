7cf0a20c2c577de68a223a2d1ce2a674763a53fa
Brian Bridges <brian.brdgs@gmail.com>
2013-02-22 18:00:13 -0600

    changed require for cron

--- script/noaa_discussions.rb
+++ script/noaa_discussions.rb
@@ -1,9 +1,12 @@
-Dir[Dir.pwd + "/" + "lib/*.rb"].each { |file| require file }
+feed_path = "/var/www/vhosts/brianlbridges.com/httpdocs/grumpyrainbow.com/feeds/"
+
+Dir.glob("#{feed_path}noaa-discussions/***").each do |file|
+  require file if file.include?(".rb")
+end
 
 require 'digest/sha1'
 require 'rss'
 
-feed_path = "/var/www/vhosts/brianlbridges.com/httpdocs/grumpyrainbow.com/feeds/"
 
 ARGV[0] = "HPC"
 
@@ -41,12 +44,12 @@ discussion_hash.keys.each do |key|
 
   new_rss_sha = Digest::SHA1.hexdigest rss.to_s
 
-  file = File.open("#{feed_path}#{ARGV[0].downcase}/#{discussion_id}.rss", "r")
+  file = File.open("#{feed_path}#{ARGV[0].downcase}/#{discussion_id}.atom", "r")
   old_rss_sha = Digest::SHA1.hexdigest file.read
   file.close
 
   if new_rss_sha != old_rss_sha
-    file = File.open("#{feed_path}#{ARGV[0].downcase}/#{discussion_id}.rss", "w") do |f|
+    file = File.open("#{feed_path}#{ARGV[0].downcase}/#{discussion_id}.atom", "w") do |f|
       f.write(rss)
       f.close
     end
